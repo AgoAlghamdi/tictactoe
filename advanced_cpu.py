03af03bf35c13b042517468804db714f49976af7
AgoAlghamdi <abdullah_017@outlook.com>
2025-07-26 23:31:13 +0300

    finished! approx 3 days

---
+++ advanced_cpu.py
@@ -0,0 +1,55 @@
+import random
+def advance_cpu(pos):
+    if (pos[2]=='X' and pos[3]=='X' and pos[1]=='1'):
+        return pos.update({1:'O'})
+    elif (pos[1]=='X' and pos[3]=='X' and pos[2]=='2'):
+        return pos.update({2:'O'})
+    elif (pos[2]=='X' and pos[1]=='X' and pos[3]=='3'):
+        return pos.update({3:'O'})
+    elif (pos[6]=='X' and pos[5]=='X' and pos[4]=='4'):
+        return pos.update({4:'O'})
+    elif (pos[4]=='X' and pos[6]=='X' and pos[5]=='5'):
+        return pos.update({5:'O'})
+    elif (pos[4]=='X' and pos[5]=='X' and pos[6]=='6'):
+        return pos.update({6:'O'})
+    elif (pos[8]=='X' and pos[9]=='X' and pos[7]=='7'):
+        return pos.update({7:'O'})
+    elif (pos[7]=='X' and pos[9]=='X' and pos[8]=='8'):
+        return pos.update({8:'O'})
+    elif (pos[7]=='X' and pos[8]=='X' and pos[9]=='9'):
+        return pos.update({9:'O'})
+    elif (pos[4]=='X' and pos[7]=='X' and pos[1]=='1'):
+        return pos.update({1:'O'})
+    elif (pos[1]=='X' and pos[7]=='X' and pos[4]=='4'):
+        return pos.update({4:'O'})
+    elif (pos[4]=='X' and pos[1]=='X' and pos[7]=='7'):
+        return pos.update({7:'O'})
+    elif (pos[8]=='X' and pos[5]=='X' and pos[2]=='2'):
+        return pos.update({2:'O'})
+    elif (pos[2]=='X' and pos[8]=='X' and pos[5]=='5'):
+        return pos.update({5:'O'})
+    elif (pos[2]=='X' and pos[5]=='X' and pos[8]=='8'):
+        return pos.update({8:'O'})
+    elif (pos[6]=='X' and pos[9]=='X' and pos[3]=='3'):
+        return pos.update({3:'O'})
+    elif (pos[3]=='X' and pos[9]=='X' and pos[6]=='6'):
+        return pos.update({6:'O'})
+    elif (pos[3]=='X' and pos[6]=='X' and pos[9]=='9'):
+        return pos.update({9:'O'})
+    elif (pos[5]=='X' and pos[9]=='X' and pos[1]=='1'):
+        return pos.update({1:'O'})
+    elif (pos[1]=='X' and pos[9]=='X' and pos[5]=='5'):
+        return pos.update({5:'O'})
+    elif (pos[1]=='X' and pos[5]=='X' and pos[9]=='9'):
+        return pos.update({9:'O'})
+    elif (pos[5]=='X' and pos[7]=='X' and pos[3]=='3'):
+        return pos.update({3:'O'})
+    elif (pos[3]=='X' and pos[7]=='X' and pos[5]=='5'):
+        return pos.update({5:'O'})
+    elif (pos[3]=='X' and pos[5]=='X' and pos[7]=='7'):
+        return pos.update({7:'O'})
+    else:
+        choice=random.randrange(1,9)
+        if (pos[choice]=='X') or (pos[choice]=='O'):
+            return advance_cpu(pos)
+        return pos.update({choice:'O'})
---
+++ main.py
@@ -0,0 +1,62 @@
+import random
+from advanced_cpu import advance_cpu
+
+
+pos={1:'1', 2:'2', 3:'3',4:'4',5:'5', 6:'6',7:'7',8:'8',9:'9'}
+
+
+def cpu(pos):
+    choice=random.randrange(1,9)
+    if (pos[choice]=='X') or (pos[choice]=='O') :
+        return cpu(pos)
+    return pos.update({choice:'O'})
+
+
+
+        
+def win_condition(pos):
+    values=list(pos.values())
+    if (pos[1]=='X' and pos[2]=='X' and pos[3]=='X') or (pos[1]=='X' and pos[4]=='X' and pos[7]=='X')or (pos[2]=='X' and pos[5]=='X' and pos[8]=='X')or (pos[3]=='X' and pos[5]=='X' and pos[7]=='X')or (pos[4]=='X' and pos[5]=='X'and pos[6]=='X') or (pos[7]=='X' and pos[8]=='X' and pos[9]=='X') or (pos[1]=='X' and pos[5]=='X'and pos[9]=='X') or (pos[3]=='X' and pos[6]=='X'and pos[9]=='X') :
+        return print('you Win!')
+    elif (pos[1]=='O' and pos[2]=='O' and pos[3]=='O') or (pos[1]=='O' and pos[4]=='O' and pos[7]=='O')or (pos[2]=='O' and pos[5]=='O' and pos[8]=='O')or (pos[3]=='O' and pos[5]=='O' and pos[7]=='O')or (pos[4]=='O' and pos[5]=='O'and pos[6]=='O') or (pos[7]=='O' and pos[8]=='O' and pos[9]=='O') or (pos[1]=='O' and pos[5]=='O'and pos[9]=='O')or (pos[3]=='O' and pos[6]=='O'and pos[9]=='O') :
+        return print('you Lose!')
+    
+    elif (values.count('O')>=4) and (values.count('X')>=4):
+        return print('Tie')
+
+    return True    
+    
+        
+        
+
+
+def draw(pos):
+    return print(f' | {pos[1]} | {pos[2]} | {pos[3]} | \n | {pos[4]} | {pos[5]} | {pos[6]} | \n | {pos[7]} | {pos[8]} | {pos[9]} |')
+
+def user_input():
+    try:
+        ans=input('choose a space: ')
+        if (len(ans)>1) or (pos[int(ans)]=='X') or (pos[int(ans)]=='O'):
+            print('invalid input!')
+            return user_input()
+        return int(ans)
+    except ValueError:
+        print('only numbers!')
+        return user_input()
+
+
+def main():
+    draw(pos)
+    while win_condition(pos):    
+       
+
+        pos.update({user_input():'X'})
+        
+        advance_cpu(pos) #hard mode
+        #cpu(pos) #easy mode
+        draw(pos)
+
+
+if __name__=='__main__':
+
+    main()
